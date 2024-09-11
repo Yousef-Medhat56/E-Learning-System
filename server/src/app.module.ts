6b52b8cc66f504a9aa676acd208d251b8f55dd93
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2024-09-11 11:32:22 +0300

    config(server): setup redis

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,13 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from 'nestjs-prisma';
+import { UpstashRedisModule } from 'nestjs-upstash-redis';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    PrismaModule.forRoot(),
+    // PRISMA
+    PrismaModule.forRoot({ isGlobal: true }),
+    // REDIS
+    UpstashRedisModule.forRoot({
+      url: process.env.UPSTASH_REDIS_URL,
+      token: process.env.UPSTASH_REDIS_TOKEN,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
