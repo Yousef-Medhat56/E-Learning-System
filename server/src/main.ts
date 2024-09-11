b1bbc423986ea524a7b727cc8e5121a6658824f5
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2024-09-11 16:56:37 +0300

    docs(server): setup swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,11 @@ async function bootstrap() {
   const logger = new Logger();
 
   app.useGlobalPipes(new ValidationPipe());
+
+  // SWAGGER
+  const config = new DocumentBuilder().setTitle('E-Learning API').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(port, () => {
     logger.log(`Listening on port ${port}`);
