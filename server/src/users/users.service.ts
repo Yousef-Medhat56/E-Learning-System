a5e731d0be9a8f9f936633ce445d8d678a898649
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2024-09-12 11:34:54 +0300

    fix(server): check if the user already exists

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+} from '@nestjs/common';
 import { ActivateUserDto, CreateUserDto } from './dto/users.dto';
 import { AuthService } from 'src/auth/auth.service';
 import { PrismaService } from 'nestjs-prisma';
@@ -12,8 +16,23 @@ export class UsersService {
 
   async signup(user: CreateUserDto) {
     try {
+      // check if the user already exists
+      const existedUser = await this.prisma.user.findUnique({
+        where: { email: user.email },
+      });
+
+      if (existedUser) {
+        throw new ConflictException('User already exists');
+      }
+    } catch (error) {
+      throw error;
+    }
+
+    try {
+      // create activation token and code
       const { token, activationCode } =
         await this.authService.createActivationToken(user);
+
       return {
         activationCode: activationCode,
         activationToken: token,
@@ -25,6 +44,7 @@ export class UsersService {
 
   async activate(activateUserDto: ActivateUserDto) {
     try {
+      // get the user data from the activation token
       const user = await this.authService.activate(activateUserDto);
       //create new user
       const createdUser = await this.prisma.user.create({
@@ -32,6 +52,10 @@ export class UsersService {
       });
       return createdUser;
     } catch (error) {
+      //check if the error reason that user already exists
+      if (error.code === 'P2002') {
+        throw new ConflictException('User already exists');
+      }
       throw new BadRequestException();
     }
   }
