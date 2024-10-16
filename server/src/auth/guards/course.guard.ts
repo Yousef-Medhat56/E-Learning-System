7f3b6c29dcf96e15aae86f6d1ebb654349a329c9
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2024-10-16 17:31:22 +0300

    feat(server/courses): add CourseGuard to check if the user purchased the course or not

---
+++ server/src/auth/guards/course.guard.ts
@@ -0,0 +1,45 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { AuthRequest } from '../interfaces/auth.interface';
+import { PrismaService } from 'nestjs-prisma';
+
+@Injectable()
+export class CourseGuard implements CanActivate {
+  constructor(private prisma: PrismaService) {}
+
+  private getCourseId(url: string) {
+    // split the url
+    const requestUrlSplit = url.split('/');
+    const courseIdIndex = requestUrlSplit.indexOf('courses') + 1;
+    const courseId = requestUrlSplit[courseIdIndex];
+    return courseId;
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest() as AuthRequest;
+
+    // get user id
+    const { id } = request.user;
+
+    // user data
+    const userData = await this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    const courseId = this.getCourseId(request.url);
+
+    // check if the user purchased this course
+    const isCoursePurchased = userData.purchasedCoursesIDs.includes(courseId);
+
+    if (isCoursePurchased) return true;
+    throw new UnauthorizedException(
+      'Purchase the course to access this content',
+    );
+  }
+}
