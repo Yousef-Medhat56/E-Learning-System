3a537f0471c5900ac84d4e03a7f46610cbb55027
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2024-10-16 19:07:23 +0300

    refactor(server/courses): update CourseGuard to just check if the user purchased the course

--- server/src/auth/guards/course.guard.ts
+++ server/src/auth/guards/course.guard.ts
@@ -3,7 +3,6 @@ import {
   CanActivate,
   ExecutionContext,
   UnauthorizedException,
-  BadRequestException,
 } from '@nestjs/common';
 import { AuthRequest } from '../interfaces/auth.interface';
 import { PrismaService } from 'nestjs-prisma';
@@ -12,14 +11,12 @@ import { PrismaService } from 'nestjs-prisma';
 export class CourseGuard implements CanActivate {
   constructor(private prisma: PrismaService) {}
 
-  private getCourseAndSectionIds(url: string) {
+  private getCourseId(url: string) {
     // split the url
     const requestUrlSplit = url.split('/');
     const courseIdIndex = requestUrlSplit.indexOf('courses') + 1;
     const courseId = requestUrlSplit[courseIdIndex];
-    const sectionIdIndex = requestUrlSplit.indexOf('sections') + 1;
-    const sectionId = requestUrlSplit[sectionIdIndex];
-    return { courseId, sectionId };
+    return courseId;
   }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
@@ -35,29 +32,14 @@ export class CourseGuard implements CanActivate {
       },
     });
 
-    const { courseId, sectionId } = this.getCourseAndSectionIds(request.url);
-
-    const courseSectionsIDs = await this.prisma.course.findUnique({
-      where: { id: courseId },
-      select: {
-        sections: {
-          select: {
-            id: true,
-          },
-        },
-      },
-    });
+    const courseId = this.getCourseId(request.url);
 
     // check if the user purchased this course
     const isCoursePurchased = userData.purchasedCoursesIDs.includes(courseId);
 
     if (isCoursePurchased) {
-      //check if the section exists in the course
-      const sectionInCourse = courseSectionsIDs.sections.find(
-        (section) => section.id == sectionId,
-      );
-      if (sectionInCourse) return true;
-      throw new BadRequestException('Invalid section Id');
+      request['courseId'] = courseId;
+      return true;
     }
     throw new UnauthorizedException(
       'Purchase the course to access this content',
